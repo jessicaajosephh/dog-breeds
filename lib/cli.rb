a9f2b425effc45c0e4b7c7ef14b625751d05ac68
Jessica Joseph <jessicaajosephh@gmail.com>
2021-02-23 16:25:49 -0500

    created a greeting method for when the user first enters the app

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,6 +4,13 @@ class CLI
         greeting
     end
 
+    def greeting
+        puts "Welcome! Start searching for dog breeds now!"
+        puts "To search for dog breeds, enter 'start'"
+        puts "If there is nothing you would like to do at the moment, enter 'exit'"
+    end   
+         
+
     
 
 
