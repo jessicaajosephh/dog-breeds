a962b612f6b586e5f54ef9c1da798af1178a53e4
Jessica Joseph <jessicaajosephh@gmail.com>
2021-02-23 16:36:11 -0500

    added a get_breed_data method so that when a new API is called it will call on the data from this method

--- lib/api.rb
+++ lib/api.rb
@@ -4,6 +4,12 @@ class API
         @url = "https://api.thedogapi.com/v1/breeds"
     end
 
+    def get_breed_data
+        breed_hash = HTTParty.get(@url)
+        breed_array = breed_hash
+        self.create_breed_objects
+    end
+
 
 
 end
