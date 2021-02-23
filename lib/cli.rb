fc1c327f60c6d129a374f95638c7cf0f265d9e0c
Jessica Joseph <jessicaajosephh@gmail.com>
2021-02-23 17:31:32 -0500

    added puts statements to make it look nicer when running the program

--- lib/cli.rb
+++ lib/cli.rb
@@ -10,15 +10,21 @@ class CLI
     end
 
     def greeting
+        puts "----------------------------------------------------------------------------"
+        puts ""
         puts "Welcome! Start searching for dog breeds now!"
-        puts "To search for dog breeds, enter 'begin'"
-        puts "If there is nothing you would like to do at the moment, enter 'exit'"
+        puts ""
+        puts "--To search for dog breeds, enter 'breeds'"
+        puts ""
+        puts "--If there is nothing you would like to do at the moment, enter 'exit'"
+        puts ""
+        puts "----------------------------------------------------------------------------"
     end   
 
     def menu
         input = gets.strip.downcase
 
-        if input == "begin"
+        if input == "breeds"
             list_of_breeds
         elsif input == "exit"
             goodbye
@@ -28,12 +34,18 @@ class CLI
     end
 
     def goodbye
+        puts ""
+        puts "---------------------------------------" 
         puts "Goodbye! Thanks for stopping in!"
+        puts "---------------------------------------"
     end
 
    
     def list_of_breeds
+        puts "----------------------------------------------------------"
+        puts ""
         puts "Select which breed you would like information on"
+        puts ""
         DogBreeds.all.each_with_index do |breed, index|
             puts "#{index + 1}. #{breed.name}"
         end
@@ -44,6 +56,7 @@ class CLI
     def breed_selection(breed)
         breed = DogBreeds.find_by_name(breed)
         breed.each do |breed_info|
+            puts "--------------------------------------------------------------------"
             puts "Name of Breed: #{breed_info.name}"
             puts "Weight: #{breed_info.weight}"
             puts "Height: #{breed_info.height}"
@@ -51,6 +64,10 @@ class CLI
             puts "Breed Group: #{breed_info.breed_group}"
             puts "Life Span: #{breed_info.life_span}"
             puts "Temperament: #{breed_info.temperament}"
+            puts "--------------------------------------------------------------------"
+            puts ""
+            puts "To continue searching for breeds, enter 'breeds'."
+            puts "If there is nothing else you would like to do, enter 'exit'."
             menu
         end
 
