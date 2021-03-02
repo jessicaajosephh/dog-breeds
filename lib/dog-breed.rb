a30cb43ec0a09cc462039dc0e7a4cef8fd808555
Jessica Joseph <jessicaajosephh@gmail.com>
2021-03-02 13:15:42 -0500

    removed comments from DogBreed class"

--- lib/dog-breed.rb
+++ lib/dog-breed.rb
@@ -14,11 +14,7 @@ class DogBreed
         @@all
     end
 
-    #def self.unique_origin(origin)
-     #   self.all.find do |breed|
-      #      breed.origin.downcase == origin
-       # end
-    #end
+   
     #binding.pry
 
     def self.find_by_name(name)
